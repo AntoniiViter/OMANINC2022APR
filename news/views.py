d0750d9718e5619ee0e3faed39dd057b4bbaaa8a
Antoniy <viterantoniy20@gmail.com>
2022-04-26 13:07:40 +0200

    OMANINC2022

--- news/views.py
+++ news/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
-from .forms import TodoForm
+from .forms import NewsForm
 from .models import News
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
@@ -48,16 +48,16 @@ def logoutuser(request):
 @login_required
 def createnews(request):
     if request.method=='GET':
-        return render(request, 'news/createnews.html', {'form': TodoForm()})
+        return render(request, 'news/createnews.html', {'form': NewsForm()})
     else:
         try:
-            form = TodoForm(request.POST)
+            form = NewsForm(request.POST)
             newnews = form.save(commit=False)
-            newnews.todocreator = request.user
+            newnews.newscreator = request.user
             newnews.save()
             return redirect('currentnews')
         except ValueError:
-            return render(request, 'news/createnews.html', {'form': TodoForm(), 'error': 'Передано неправильні дані. Повторіть спробу'})
+            return render(request, 'news/createnews.html', {'form': NewsForm(), 'error': 'Передано неправильні дані. Повторіть спробу'})
 
 @login_required
 def currentnews(request):
@@ -65,33 +65,33 @@ def currentnews(request):
     return render(request, 'news/currentnews.html', {'news': news})
 
 @login_required
-def viewnews(request, todo_id):
+def viewnews(request, news_id):
     viewnews = get_object_or_404(News, pk=news_id, newscreator=request.user)
     if request.method == 'GET':
-        form = TodoForm(instance=viewnews)
+        form = NewsForm(instance=viewnews)
         return render(request, 'news/viewnews.html', {'viewnews': viewnews, 'form': form})
     else:
         try:
-            form = TodoForm(request.POST, instance=viewnews)
+            form = NewsForm(request.POST, instance=viewnews)
             form.save()
             return redirect('currentnews')
         except ValueError:
-            return render(request, 'news/createnews.html', {'form': TodoForm(), 'error': 'Передано неправильні дані. Повторіть спробу'})
+            return render(request, 'news/createnews.html', {'form': NewsForm(), 'error': 'Передано неправильні дані. Повторіть спробу'})
 
-def archivenews(request, todo_id):
+def archivenews(request, news_id):
     archivenews = get_object_or_404(News, pk=news_id, newscreator=request.user)
     if request.method == 'POST':
-        archivenews.datecompleted = timezone.now()
+        archivenews.datearchived = timezone.now()
         archivenews.save()
         return redirect('currentnews')
 
-def deletenews(request, todo_id):
+def deletenews(request, news_id):
     deletednews = get_object_or_404(News, pk=news_id, newscreator=request.user)
     if request.method == 'POST':
         deletednews.delete()
         return redirect('currentnews')
 
-def deletenews_archives(request, todo_id):
+def deletenews_archives(request, news_id):
     deletednews = get_object_or_404(News, pk=news_id, newscreator=request.user)
     if request.method == 'POST':
         deletednews.delete()
